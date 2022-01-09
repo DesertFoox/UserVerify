afe9d5ee1b614d5c5f0443046cd5e1ebe47ab06a
desertfoox <salar.nili097@gmail.com>
2022-01-09 09:47:16 +0330

    add/architecture

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import UnAuthenticationApp from "./UnAuthenticationApp/UnAuthenticationApp";
+
+function App() {
+  return (
+    <div className="App">
+      <UnAuthenticationApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/UnAuthenticationApp/UnAuthenticationApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UnAuthenticationApp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UnAuthenticationApp
